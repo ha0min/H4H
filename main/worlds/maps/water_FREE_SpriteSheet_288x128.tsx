f1637677d46b98204331aaf2e5bcd59f485a3188
M Mohana Satyanarayana <mohana4984@gmail.com>
2024-02-18 03:14:08 -0800

    conflict resolved

--- main/worlds/maps/water_FREE_SpriteSheet_288x128.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="water_FREE_SpriteSheet_288x128" tilewidth="32" tileheight="32" tilecount="17280" columns="288">
- <image source="../../../../water_FREE_SpriteSheet_288x128.png" width="9216" height="1920"/>
-</tileset>
