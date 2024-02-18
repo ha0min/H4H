6a0fc0fb4dcde3541012cdb1337c1275189a8ad4
Lai <57856015+kingofbug@users.noreply.github.com>
2024-02-18 02:43:47 -0800

    fix: complete the villager

--- main/events/villager.ts
+++ main/events/villager.ts
@@ -9,12 +9,32 @@ import { RpgEvent, EventData, RpgPlayer } from '@rpgjs/server'
 })
 export default class VillagerEvent extends RpgEvent {
     onInit() {
-        this.setGraphic('female')
+        this.setGraphic('father_friend_wife')
     }
     async onAction(player: RpgPlayer) {
-        await player.showText('I give you 10 gold.', {
-            talkWith: this
-        })
-        player.gold += 10
+        const response = await player.showChoices('Hello, Zoe! It\'s been a long time since I\'ve seen you.', [
+            { text: 'Where am I?', value: 'yes' },
+            { text: 'Please leave me alone.', value: 'no' }
+        ], { talkWith: this });
+
+        switch (response?.value) {
+            case 'yes':
+                // Provide the game instructions here
+                await player.showText("You are now in your trauma zone.\n\n- Use arrow keys to move around.\n- Interact with objects and people using the 'Spacebar'.\n", {
+                    talkWith: this
+                });
+                await player.showText("Explore carefully and interact with everyone you meet. Good luck!", {
+                    talkWith: this
+                });
+                break;
+            case 'no':
+                await player.showText("Of course! Bye, Zoe.", {
+                    talkWith: this
+                });
+                break;
+            default:
+                console.log('Unexpected choice value or no choice made.');
+                break;
+        }
     }
 } 
